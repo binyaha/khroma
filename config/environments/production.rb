434b12d4ac3aee68c0aa594dfab1cd3f797d8f59
binyaha <yahabin@gmail.com>
2018-04-05 17:28:21 +0800

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,11 +49,11 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
   address: "smtp.gmail.com",
   port: 587,
-  domain: 'gmail.com',
+  domain: ENV["GMAIL_DOMAIN"],
   authentication: "plain",
   enable_starttls_auto: true,
-  user_name: 'khromago@gmail.com',
-  password: 'khroma1234'
+  user_name: ENV["GMAIL_USERNAME"],
+  password: ENV["GMAIL_PASSWORD"]
   }
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
