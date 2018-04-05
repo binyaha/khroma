66d583aa54ff8e4d1f66e7942dfa9130728dee7a
binyaha <yahabin@gmail.com>
2018-04-05 16:35:43 +0800

    mail test1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,13 +39,22 @@ Rails.application.configure do
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
-  config.action_mailer.default_url_options = {:host => 'yahabin@gmail.com'}
+  config.action_mailer.default_url_options = {:host => 'www.khroma-go.com'}
+  Rails.application.routes.default_url_options[:host] = 'www.khroma-go.com'
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+
   config.action_mailer.smtp_settings = {
-  :address => "127.0.0.1",
-  :port    => 25,
-  :domain  => 'yahabin@gmail.com'
-}
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: ENV["GMAIL_DOMAIN"],
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: ENV["GMAIL_USERNAME"],
+  password: ENV["GMAIL_PASSWORD"]
+  }
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
