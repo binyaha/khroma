99d0ad20b0725be4e96c9b3a34eeb2147a556687
binyaha <yahabin@gmail.com>
2018-04-09 22:01:00 +0800

    add yml to config

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,7 @@ set :deploy_to, '/home/deploy/khroma'
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml", "config/secrets.yml", "config/google.yml", "config/facebook.yml"
+append :linked_files, "config/database.yml", "config/secrets.yml", "config/google.yml", "config/facebook.yml", "config/cloudinary.yml"
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/uploads"
